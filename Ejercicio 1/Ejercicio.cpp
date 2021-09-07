70883a923f640c2e195de7aab9f2e8b287838a91
Grisel Yavier Sañudo Lopez <griselgylo@gmail.com>
2021-09-06 21:14:51 -0500

    Hola Mundo y primeros ejercicios

---
+++ Ejercicio 1/Ejercicio.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std :: cout << "Introduzca un numero: ";
+    int x {};
+    std :: cin >> x;
+    std :: cout << "Ingresaste " << x << '\ n'; //Termina con advertencia
+    return 0;
+}
---
+++ Ejercicio 2/Ejercicio2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std :: cout << "Introduzca un numero: ";
+    int x {};
+    std :: cin >> x;
+    std :: cout << "El doble del numero es: " << x * 2 << '\n';
+    return 0;
+}
