5edbafb075c81d1cfc8e131b6d88fc02da90fd5d
Grisel Yavier Sañudo Lopez <griselgylo@gmail.com>
2021-10-04 21:23:23 -0500

    +Se me olvidó como estaban acomodados los capitulos (el capitulo 3 me lo comí) +Más ejerciciozZzzZzz

---
+++ Capitulo 4/Ejercicio 13/ejercicio13.cpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+#include <iostream>
+
+int main(){
+    std::int_fast8_t myint{65};
+    std::cout << myint;
+
+    return 0;
+}
---
+++ Capitulo 4/Ejercicio 14/ejercicio14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    std::cout << 5.0f << "\n";
+    std::cout << 6.7f << "\n";
+    std::cout << 9876543.21 << "\n";
+    std::cout << "";
+    std::cout << 9.87654321f << "\n";
+    std::cout << 987.654321f << "\n";
+    std::cout << 987654.321f << "\n";
+    std::cout << 0.0000987654321f << "\n";
+
+    return 0;
+}
